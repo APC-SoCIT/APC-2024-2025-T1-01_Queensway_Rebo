04a1c6dc676491670028d3d34ab14b54a025277e
Julian <jester.locke11@gmail.com>
2025-04-30 03:38:10 +0800

    for checkout process and initial implementation of invoice

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,12 +1,24 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    public function index() {
-        return view('user.dashboard');
+
+ 
+    public function dashboard()
+    {
+        $orders = Order::where('user_id', Auth::id())
+                       ->with('items.product')
+                       ->get();
+    
+        return view('user.dashboard', compact('orders'));
     }
+    
+    
+    
 }
