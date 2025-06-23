18b5814febe2bd261bb18c7c684149e983dfdefc
Kirk Gozon <kbgozon@student.apc.edu.ph>
2025-06-24 03:09:41 +0800

    update configs

--- .devcontainer/init-db.sh
+++ .devcontainer/init-db.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
-# Wait for MariaDB to be ready
-until mysqladmin ping -h "localhost" --silent; do
-  echo "Waiting for MariaDB to be ready..."
+# Wait for MySQL to be ready
+until mysqladmin ping -h db -uroot -proot --silent; do
+  echo "Waiting for MySQL to be ready..."
   sleep 2
 done
 
-echo "MariaDB is ready. Importing ecommerce.sql..."
-mysql -uroot -proot ecommerce < /workspace/ecommerce.sql || echo "SQL already imported or error."
+echo "Importing ecommerce.sql..."
+mysql -h db -uroot -proot ecommerce < /workspace/ecommerce.sql || echo "SQL already imported or error."
